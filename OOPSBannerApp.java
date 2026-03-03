6d77f5878351ef67fd4307d35d4f4b6b34e52b67
athiraanup007-del <athiraanup007@gmail.com>
2026-03-03 09:21:02 +0530

    Add files via upload

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,8 +1,38 @@
 public class OOPSBannerApp {
-
     public static void main(String[] args) {
-
-        System.out.println("OOPS");
-
+        String[] O = {
+                " ***** ",
+                "*     *",
+                "*     *",
+                "*     *",
+                "*     *",
+                "*     *",
+                " ***** "
+        };
+        String[] P = {
+                " ******",
+                "*     *",
+                "*     *",
+                " ******",
+                "*      ",
+                "*      ",
+                "*      "
+        };
+        String[] S = {
+                " ******",
+                "*      ",
+                "*      ",
+                " ***** ",
+                "      *",
+                "      *",
+                " ******"
+        };
+        String[] lines = new String[7];
+        for (int i = 0; i < 7; i++) {
+            lines[i] = String.join("   ", O[i], O[i], P[i], S[i]);
+        }
+        for (String line : lines) {
+            System.out.println(line);
+        }
     }
 }
